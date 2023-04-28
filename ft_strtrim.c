72ec726aee3cd667956d8411ec0c6cd61555f01c
Frederica Maria Gomes Ferreira <fgomes-f@c1r6s6.42porto.com>
2023-04-28 18:00:29 +0100

    f

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,63 +6,56 @@
 /*   By: fgomes-f <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 13:21:21 by fgomes-f          #+#    #+#             */
-/*   Updated: 2023/04/27 16:39:46 by fgomes-f         ###   ########.fr       */
+/*   Updated: 2023/04/28 17:30:46 by fgomes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+int	ft_is_set(char c, char const *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_build_string(char *string, char const *s1, size_t i, size_t j)
+{
+	size_t	x;
+
+	x = 0;
+	if (!string)
+		return (NULL);
+	while (j > i)
+		string[x++] = s1[i++];
+	string[x] = '\0';
+	return (string);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 	size_t	j;
-	size_t	x;
 	char	*string;
 
 	i = 0;
-	x = 0;
-	j = 0;
 	if (!set || !*set)
 		return (ft_strdup(s1));
-	if (!set)
-		return (ft_strdup(""));
 	if (!s1)
 		return (NULL);
-	if (ft_strlen(s1) == 0)
+	while (s1[i] && ft_is_set(s1[i], set))
+			i++;
+	j = ft_strlen(s1) - 1;
+	while (j > i && s1[j] && ft_is_set(s1[j], set))
+		j--;
+	if (j < i)
 		return (ft_strdup(""));
-	while (set[j] != '\0')
-	{
-		j = 0;
-		while (s1[i] != set[j] && set[j] != '\0')
-			j++;
-		i++;
-	}
-	i--;
-	if (s1[i] == '\0')
-		return (ft_strdup(""));
-	j = 0;
-	i--;
-	x = ft_strlen(s1) - 1;
-	while (set[j] != '\0')
-	{
-		j = 0;
-		while (s1[x] != set[j] && set[j] != '\0')
-			j++;
-		x--;
-	}
-	x++;
-	string = malloc(sizeof(char) * (x - i) + 1);
-	if (!string)
-		return (NULL);
-	j = 0;
-	i++;
-	while (x >= i)
-	{
-		string[j] = s1[i];
-		j++;
-		i++;
-	}
-	string[j] = '\0';
-	return (string);
+	j++;
+	string = malloc(sizeof(char) * (j - i) + 1);
+	return (ft_build_string(string, s1, i, j));
 }
