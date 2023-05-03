487b5e2c4c150098a8ebbe14c24312d9d2d7e28a
Frederica Maria Gomes Ferreira <fgomes-f@c1r6s2.42porto.com>
2023-05-03 16:05:49 +0100

    h

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgomes-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: fgomes-f <fgomes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 15:11:46 by fgomes-f          #+#    #+#             */
-/*   Updated: 2023/05/02 17:53:42 by fgomes-f         ###   ########.fr       */
+/*   Updated: 2023/05/03 14:28:40 by fgomes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,15 +46,15 @@ size_t	ft_count_letters(char const *s, char c, size_t j)
 	x = 0;
 	i = 0;
 	while (s[i] && s[i] == c)
-                i++;
-        while (s[i] && x != j)
-        {
-                if (s[i] != c)
-                        x++;
-                while (s[i] && s[i] != c)
-                        i++;
-                while (s[i] && s[i] == c)
-                        i++;
+		i++;
+	while (s[i] && x != j)
+	{
+		if (s[i] != c)
+			x++;
+		while (s[i] && s[i] != c)
+			i++;
+		while (s[i] && s[i] == c)
+			i++;
 	}
 	x = 0;
 	y = 0;
@@ -66,50 +66,53 @@ size_t	ft_count_letters(char const *s, char c, size_t j)
 	return (y);
 }
 
-
-char	**ft_split(char const *s, char c)
+char	**ft_write_words(char **array, size_t n_of_words, const char *s, char c)
 {
 	size_t	i;
-	char	**array_of_strings;
-	size_t	j;
 	size_t	x;
-	size_t	number_of_words;
+	size_t	j;
 
 	i = 0;
 	x = 0;
-	number_of_words = ft_count_words(s, c);
-	array_of_strings = malloc(sizeof(char*) * number_of_words);
-	if (!array_of_strings)
-		return (NULL);
-	while (i <= number_of_words)
-	{
-		array_of_strings[i] = malloc(sizeof(char) * (ft_count_letters(s, c, i) + 1));	
-		printf("%zu\n", ft_count_letters(s, c, i));
-		i++;
-	}
-	if (!array_of_strings[i])
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (i <= number_of_words)
+	while (i < n_of_words)
 	{
 		j = 0;
 		while (s[x] && s[x] == c)
 			x++;
 		while (s[x] && s[x] != c)
 		{
-			array_of_strings[i][j] = s[x];
+			array[i][j] = s[x];
 			x++;
 			j++;
 		}
-		array_of_strings[i][j] = '\0';
-	i++;
+		array[i][j] = '\0';
+		i++;
 	}
-//	array_of_strings[number_of_words] = '\0';
-	return (array_of_strings);
+	array[n_of_words] = NULL;
+	return (array);
 }
 
-int	main(void)
+char	**ft_split(char const *s, char c)
+{
+	size_t	i;
+	char	**array;
+	size_t	n_of_words;
+
+	i = 0;
+	n_of_words = ft_count_words(s, c);
+	array = malloc(sizeof(char *) * (n_of_words + 1));
+	if (!array)
+		return (NULL);
+	while (i < n_of_words)
+	{
+		array[i] = malloc(sizeof(char) * (ft_count_letters(s, c, i) + 1));
+		i++;
+	}
+	i = 0;
+	return (ft_write_words(array, n_of_words, s, c));
+}
+
+/*int	main(void)
 {
 	size_t	i;
 	char	*s1;
@@ -117,13 +120,14 @@ int	main(void)
 	char	**test;
 
 	c = ' ';
-	s1 = "array b carro";
+	s1 = "     ";
+//	s1 = "array b carro";
 	test = ft_split(s1, c);
 	i = 0;
-	printf("%s\n", test[i]);
+//	printf("%s\n", test[i]);
 	while (i <= ft_count_words(s1, c))
 	{
 		printf("%s\n", test[i]);
 		i++;
 	}
-}
+}*/
